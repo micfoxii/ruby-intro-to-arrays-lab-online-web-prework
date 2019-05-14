bfb59e6be2f393e4a4aa11ea8801c69262578dea
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-14 23:55:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -22,8 +22,8 @@ def first_element_with_array_methods(first_country)
   @south_east_asia.first 
 end
 
-def last_element_with_array_methods
-  Array.last
+def last_element_with_array_methods(last_country)
+  @south_east_asia.last
 end
 
 def length_of_array
