95224ab07a16b4a6f4be264001414c166afaa321
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-14 23:56:47 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array 
+  Array.new 
+end
+
+def array_with_two_elements
+  Array.new(2)
+end
+
+def first_element(my_first_element)
+  @taylor_swift.shift
+end
+
+def third_element(my_third_element)
+  @taylor_swift[2]
+end
+
+def last_element(my_last_element)
+  @taylor_swift[-1]
+end
+
+def first_element_with_array_methods(first_country)
+  @south_east_asia.first 
+end
+
+def last_element_with_array_methods(last_country)
+  @south_east_asia.last
+end
+
+def length_of_array(lengeth)
+ @programming_languages.length
+end
